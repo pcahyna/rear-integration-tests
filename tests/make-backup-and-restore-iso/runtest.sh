1214afb774b050fda4fa862dc78a2185c71928d0
Pavel Cahyna <pcahyna@redhat.com>
2024-06-11 18:25:10 +0200

    Delete the sleep at the beginning
    not very useful - we need the log at the end.
    
    Also shorten the sleep at the end - longer one does not seem to have any
    benefit.

--- tests/make-backup-and-restore-iso/runtest.sh
+++ tests/make-backup-and-restore-iso/runtest.sh
@@ -107,8 +107,8 @@ BACKUP_URL=iso:///backup
 OUTPUT_URL=null
 USER_INPUT_TIMEOUT=10
 # 4gb backup limit
-PRE_RECOVERY_SCRIPT=(\"mkdir /tmp/mnt;\" \"mount $OUTPUT_DISK ${OUTPUT_SUBVOL:+-o subvol=${OUTPUT_SUBVOL}} /tmp/mnt/;\" \"modprobe brd rd_nr=1 rd_size=2097152;\" \"dd if=/tmp/mnt/$OUTPUT_FS_PATH/rear-$HOSTNAME_SHORT.iso of=/dev/ram0;\" \"umount /tmp/mnt/;\" \"sleep 300;\")
-POST_RECOVERY_SCRIPT=(\"ls -l /mnt/local/var /mnt/local/var/ARTIFACTS > /dev/console 2>&1;\" \"tail /var/lib/rear/restore/recover.backup.*.restore.log > /dev/ttyS0 2>&1;\" \"sleep 900;\")
+PRE_RECOVERY_SCRIPT=(\"mkdir /tmp/mnt;\" \"mount $OUTPUT_DISK ${OUTPUT_SUBVOL:+-o subvol=${OUTPUT_SUBVOL}} /tmp/mnt/;\" \"modprobe brd rd_nr=1 rd_size=2097152;\" \"dd if=/tmp/mnt/$OUTPUT_FS_PATH/rear-$HOSTNAME_SHORT.iso of=/dev/ram0;\" \"umount /tmp/mnt/;\")
+POST_RECOVERY_SCRIPT=(\"ls -l /mnt/local/var /mnt/local/var/ARTIFACTS > /dev/console 2>&1;\" \"tail /var/lib/rear/restore/recover.backup.*.restore.log > /dev/ttyS0 2>&1;\" \"sleep 600;\")
 ISO_FILE_SIZE_LIMIT=4294967296
 ISO_DEFAULT=automatic
 ISO_RECOVER_MODE=unattended' | tee $REAR_CONFIG" \
