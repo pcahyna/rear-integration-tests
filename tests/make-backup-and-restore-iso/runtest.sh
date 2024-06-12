3621888aa030f40bd92ec8c693e3b7e76f7a38dc
Pavel Cahyna <pcahyna@redhat.com>
2024-06-12 14:41:20 +0200

    Check backup integrity

--- tests/make-backup-and-restore-iso/runtest.sh
+++ tests/make-backup-and-restore-iso/runtest.sh
@@ -110,6 +110,7 @@ USER_INPUT_TIMEOUT=10
 PRE_RECOVERY_SCRIPT=(\"mkdir /tmp/mnt;\" \"mount $OUTPUT_DISK ${OUTPUT_SUBVOL:+-o subvol=${OUTPUT_SUBVOL}} /tmp/mnt/;\" \"modprobe brd rd_nr=1 rd_size=2097152;\" \"dd if=/tmp/mnt/$OUTPUT_FS_PATH/rear-$HOSTNAME_SHORT.iso of=/dev/ram0;\" \"umount /tmp/mnt/;\")
 POST_RECOVERY_SCRIPT=(\"ls -l /mnt/local/var /mnt/local/var/ARTIFACTS > /dev/console 2>&1;\" \"tail /var/lib/rear/restore/recover.backup.*.restore.log > /dev/ttyS0 2>&1;\" \"sleep 600;\")
 ISO_FILE_SIZE_LIMIT=4294967296
+BACKUP_INTEGRITY_CHECK=yes
 ISO_DEFAULT=automatic
 ISO_RECOVER_MODE=unattended' | tee $REAR_CONFIG" \
                 0 "Creating basic configuration file"
